0da5e525845868a32c73e75839b7412182e369d7
samik-j <jo.senczuk@gmail.com>
2020-07-09 17:14:22 +0200

    Initialise

---
+++ src/main/java/com/joanna/roomrating/RoomRatingApplication.java
@@ -0,0 +1,13 @@
+package com.joanna.roomrating;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoomRatingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoomRatingApplication.class, args);
+	}
+
+}
