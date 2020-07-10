7c35af662ebb665019d3f4503f761f3780186a45
samik-j <Perfidny12>
2020-07-10 12:49:44 +0200

    Register in eureka

--- src/main/java/com/joanna/roomrating/RoomRatingApplication.java
+++ src/main/java/com/joanna/roomrating/RoomRatingApplication.java
@@ -2,12 +2,14 @@ package com.joanna.roomrating;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class RoomRatingApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(RoomRatingApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(RoomRatingApplication.class, args);
+    }
 
 }
