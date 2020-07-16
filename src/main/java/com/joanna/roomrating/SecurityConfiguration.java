216d5e14486b49c13f14f7c2be663716a403d867
samik-j <jo.senczuk@gmail.com>
2020-07-16 14:26:54 +0200

    Cleanup

--- src/main/java/com/joanna/roomrating/SecurityConfiguration.java
+++ src/main/java/com/joanna/roomrating/SecurityConfiguration.java
@@ -1,11 +1,9 @@
 package com.joanna.roomrating;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
-@Profile("cloud")
 @Configuration
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
