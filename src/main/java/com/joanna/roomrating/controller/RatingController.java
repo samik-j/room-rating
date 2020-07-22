d35ecb3bc3a4bd381f257db30d9ac6eafdb8fbd2
samik-j <jo.senczuk@gmail.com>
2020-07-22 11:54:39 +0200

    Add security

--- src/main/java/com/joanna/roomrating/controller/RatingController.java
+++ src/main/java/com/joanna/roomrating/controller/RatingController.java
@@ -6,6 +6,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.MediaType;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -31,6 +32,7 @@ public class RatingController {
         this.ratingService = ratingService;
     }
 
+    @PreAuthorize("hasAuthority('custom_mod')")
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public List<RatingDto> getAllRatings() {
         log.info(test);
