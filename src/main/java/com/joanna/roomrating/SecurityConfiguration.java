63b9891e58f560c7637c7c050751c9315cec65b4
samik-j <jo.senczuk@gmail.com>
2020-07-22 15:01:58 +0200

    Add swagger

--- src/main/java/com/joanna/roomrating/SecurityConfiguration.java
+++ src/main/java/com/joanna/roomrating/SecurityConfiguration.java
@@ -2,16 +2,18 @@ package com.joanna.roomrating;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
-@EnableGlobalMethodSecurity(prePostEnabled=true)
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.antMatcher("/**").authorizeRequests().anyRequest().authenticated();
+    public void configure(WebSecurity http) throws Exception {
+        http.ignoring()
+            .antMatchers("/swagger-ui.html")
+            .antMatchers("/v2/api-docs");
     }
 
 }
